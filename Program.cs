34e9847194bc181b425056a96da02bba87547b90
Payton Hansen <paymeaton12@gmail.com>
2025-01-14 15:13:04 -0700

    Finished

--- Program.cs
+++ Program.cs
@@ -1,2 +1,58 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿
+Console.Clear();
+int choice;
+seasons todo;
+
+Console.WriteLine("Please Choose A Season by entering the corresponding number");
+Console.WriteLine("1. Winter");
+Console.WriteLine("2. Spring");
+Console.WriteLine("3. Summer");
+Console.WriteLine("4. Fall");
+
+while (true)
+{
+    try
+    {
+        choice = int.Parse(Console.ReadLine());
+        if (choice >= 1 && choice <= 4)
+        {
+            todo = (seasons)choice;
+            break;
+        }
+        else
+        {
+            Console.Write("Please enter a value 1-4: ");
+        }
+    }
+    catch
+    {
+        Console.Write("Please enter a valid integer: ");
+    }
+}
+Console.WriteLine(activity(todo));
+
+string activity(seasons season)
+{
+    switch (season)
+    {
+        case seasons.Winter:
+            return "Go Skiing";
+        case seasons.Spring:
+            return "Enjpoy the Blooming flowers";
+        case seasons.Summer:
+            return "Go to the beach";
+        case seasons.Fall:
+            return "Visit the Pumpkin patch";
+        default:
+            break;
+    }
+    return "";
+}
+
+public enum seasons
+{
+    Winter = 1,
+    Spring,
+    Summer,
+    Fall
+}
